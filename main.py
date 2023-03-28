beeb72513220c19129b376b320855f5d9db123a2
Domagoj <domagoj.speljko@gmail.com>
2023-03-29 01:10:29 +0200

    popravak na dictionary files i kako se zovu podaci

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from datetime import date
 korisnik = {}
-ime = input("Unesite ime korisnika:").capitalize().strip()
-prezime = input("Unesite prezime korisnika:").capitalize().strip()
-telefon = str(input("Unesite telefon korisnika:")).strip()
-email = input("Unesite email korisnika:").strip()
-korisnik[ime] = prezime, telefon, email  # Spremam unesene ime,prezime,telefon i email u riječnik korisnik
+korisnik["ime"] = input("Unesite ime korisnika:").capitalize().strip()
+korisnik["prezime"] = input("Unesite prezime korisnika:").capitalize().strip()
+korisnik["telefon"] = str(input("Unesite telefon korisnika:")).strip()
+korisnik["email"] = input("Unesite email korisnika:").strip()
 artikl = {}
-naslov = input("Unesite naslov artikla:")
-opis = input("Unesite opis artikla:")
-cijena = str(input("Unesite cijenu artikla:"))
-artikl["naslov"] = opis, cijena
+artikl["naslov"] = input("Unesite naslov artikla:")
+artikl["opis"] = input("Unesite opis artikla:")
+artikl["cijena"] = str(input("Unesite cijenu artikla:"))
 artikl["korisnik"] = korisnik
 prodaja = {}
 dan = int(input("Unesite dan isteka prodaje:"))
 mjesec = int(input("Unesite mjesec isteka prodaje:"))
 godina = int(input("Unesite godinu isteka prodaje:"))
-datum = date(godina, mjesec, dan)
-prodaja["datum"] = datum
+prodaja["datum"] = date(godina, mjesec, dan)
 prodaja["korisnik"] = korisnik
 prodaja["artikl"] = artikl
-print("Informacije o artiklu:", "\n\t Naslov:", naslov, "\n\t Opis:", opis, "\n\t Cijena:", cijena)
-print("Datum isteka prodaje:", "\n\t Dan:", dan, "\n\t Mjesec:", mjesec, "\n\t Godina:", godina)
-print("Informacije o korisniku:", "\n\t", ime,prezime, "\n\t Telefon:", telefon, "\n\t Email:", email)  #Ispisuje informacije
+print('Informacije o artiklu:')
+print('\t\t Naslov:', prodaja["artikl"]["naslov"])
+print('\t\t Opis:', prodaja["artikl"]["opis"])
+print('\t\t Cijena:', prodaja["artikl"]["cijena"])
+print('Datum isteka prodaje:')
+print(f'\t\t Dan:',prodaja["datum"].day)
+print(f'\t\t Mjesec:',prodaja["datum"].month)
+print(f'\t\t Godina:',prodaja["datum"].year)
+print('Informacije o korisniku:')
+print(f'\t\t Ime:', prodaja["korisnik"]["ime"])
+print(f'\t\t Prezime:', prodaja["korisnik"]["prezime"])
+print(f'\t\t Telefon:', prodaja["korisnik"]["telefon"])
+print(f'\t\t Email:', prodaja["korisnik"]["email"])
